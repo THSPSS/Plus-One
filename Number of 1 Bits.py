2bae348ddaa8aab19c3c3ceb16f505362ba0cda0
THSPSS <sunyeongbang98@gmail.com>
2023-03-15 23:20:48 +0900

    update better solution of Number of 1 Bits.py

--- Number of 1 Bits.py
+++ Number of 1 Bits.py
@@ -13,3 +13,14 @@ solution = Solution()
 answer = solution.hammingWeight(0o000000000000001101)
 
 print(answer)
+
+
+
+#12ms solution
+# class Solution:
+#     def hammingWeight(self, n: int) -> int:
+#         i=0
+#         while n>0:
+#             n=n&(n-1)
+#             i+=1
+#         return i
