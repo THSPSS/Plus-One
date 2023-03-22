31cf5e7d4c9e8d2c689474e0260c38acf0e4aa7b
THSPSS <sunyeongbang98@gmail.com>
2023-03-22 09:57:24 +0900

    revise search insert position

--- Search_Insert_Position.py
+++ Search_Insert_Position.py
@@ -17,6 +17,26 @@ class Solution:
                 else:
                     return i+1
 
+#revise solution
+class ReviseSolution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums) - 1
+
+        while start <= end:
+            mid = (start + end) // 2
+
+            if target == nums[mid]:
+                return mid
+
+            elif target < nums[mid]:
+                end = mid - 1
+
+            else:
+                start = mid + 1
+
+        return start
+
 
 #ohter solution runtime 27ms
 class OtherSolution:
