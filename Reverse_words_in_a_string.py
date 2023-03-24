4758208e3b4602777122657f9effffa857bcef6d
THSPSS <sunyeongbang98@gmail.com>
2023-03-24 11:21:09 +0900

    leetcode reverse words in a string

---
+++ Reverse_words_in_a_string.py
@@ -0,0 +1,13 @@
+class Solution:
+    #my solution 38ms
+    def reverseWords(self, s: str) -> str:
+        newString = ' '.join([word[::-1] for word in s.split()])
+
+        return newString
+
+    #other solution runtime 15ms
+        def OtherSolutionReverseWords(self, s: str) -> str:
+            s_list = s.split()
+            for i in range(len(s_list)):
+                s_list[i] = s_list[i][::-1]
+            return ' '.join(s_list)
