8e752119bf7752cdf1250170da2c1a2fe217b64e
THSPSS <sunyeongbang98@gmail.com>
2023-03-30 08:33:45 +0900

    leetcode missing number

---
+++ Missing_number.py
@@ -0,0 +1,9 @@
+def missingNumber(self , nums: List[int]) -> int:
+    ##first attempt
+    # for i in range(len(nums)+1):
+    #     if i not in nums:
+    #         return i
+
+    #other solution
+    TotalRange = len(nums)
+    return TotalRange * (TotalRange + 1) // 2 - sum(nums)
--- Reverse_String.py
+++ Reverse_String.py
@@ -1,18 +1,18 @@
 from typing import List
-    def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        ## first_attempt
-        #s.reverse()
-        #return s
+def reverseString(self, s: List[str]) -> None:
+    """
+    Do not return anything, modify s in-place instead.
+    """
+    ## first_attempt
+    #s.reverse()
+    #return s
 
-        #other solution
-        lp, rp = 0, len(s) - 1
+    #other solution
+    lp, rp = 0, len(s) - 1
 
-        while lp < rp:
-            s[lp], s[rp] = s[rp], s[lp]
-            lp += 1
-            rp -= 1
+    while lp < rp:
+        s[lp], s[rp] = s[rp], s[lp]
+        lp += 1
+        rp -= 1
 
-        return s
+    return s
