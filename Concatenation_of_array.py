c918ff4ffdda092fc4fa426e4d94fa543ce81fb0
THSPSS <sunyeongbang98@gmail.com>
2023-04-01 19:43:35 +0900

    Concatenation_of_Array.py

---
+++ Concatenation_of_array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        #first attemp
+        # ans = []
+        # ans = nums * 2
+        # return ans
+        #other solution
+        #return nums * 2
+
+        #using Slicing and addition
+        #return nums+nums[:]
