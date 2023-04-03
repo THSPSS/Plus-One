0cddf8554de64c2ee4cfb004d8b4aae63c2b6760
THSPSS <sunyeongbang98@gmail.com>
2023-04-03 20:18:45 +0900

    update defanging an ip address

--- Defanging_an_ip_address.py
+++ Defanging_an_ip_address.py
@@ -2,6 +2,8 @@ class Solution:
     def defangIPaddr(self, address: str) -> str:
         # first solution
         # using replace "." to "[.]"
+
+        # The replace() method replaces a specified phrase with another specified phrase.
         # return address.replace(".","[.]")
         # put () and memory reduced from 13.8 to 13.6
         return (address.replace(".", "[.]"))
