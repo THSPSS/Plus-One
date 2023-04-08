b21f349f6630cb8c5b6f57105a36fb73b6ead66e
THSPSS <sunyeongbang98@gmail.com>
2023-04-08 21:22:19 +0900

    leetcode number of good pairs

--- Number_of_good_pairs.py
+++ Number_of_good_pairs.py
@@ -35,3 +35,15 @@ class Solution:
                 repeat[v] = 1
         # return
         return num
+
+
+    def AnotherNumIdenticalPairs(self, nums: List[int]) -> int:
+        hashMap = {}
+        res = 0
+        for number in nums:
+            if number in hashMap:
+                res += hashMap[number]
+                hashMap[number] += 1
+            else:
+                hashMap[number] = 1
+        return res
