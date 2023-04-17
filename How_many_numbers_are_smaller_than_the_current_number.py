2da844d8df7efd250a9314f7fb0f0bfe1ec1c33f
THSPSS <sunyeongbang98@gmail.com>
2023-04-17 22:38:21 +0900

    leetcode how many numbers are smaller than the current number

--- How_many_numbers_are_smaller_than_the_current_number.py
+++ How_many_numbers_are_smaller_than_the_current_number.py
@@ -20,3 +20,13 @@ class Solution:
                 mapping[n] = i
 
         return [mapping[key] for key in nums]
+
+    def usingForLoopSmallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        Output = []
+        for i in range(len(nums)):
+            count = 0
+            for j in range(len(nums)):
+                if j != i and nums[j]<nums[i]:
+                   count += 1
+            Output.append(count)
+        return Output
