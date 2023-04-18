103064e42c9a0f5f73a41a50e576bc92ee46aebd
THSPSS <sunyeongbang98@gmail.com>
2023-04-19 06:27:01 +0900

    leetcode Goal_parser_Interpreter

--- Goal_parser_Interpreter.py
+++ Goal_parser_Interpreter.py
@@ -8,3 +8,16 @@ class Solution:
         }
         #and making for loop through str
         #making str
+
+    #first passed solution
+    #ohter Interpret
+    def otherInterpret(self, command: str) -> str:
+        output = ""
+        for i, c in enumerate(command):
+            if c == "G":
+                output += c
+            if c == "(" and command[i + 1] == ")":
+                output += "o"
+            if c == "(" and command[i + 1] == "a":
+                output += "al"
+        return (output)
