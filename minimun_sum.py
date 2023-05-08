0ea23bb13ad4561f220cf59f7adda82f259b9e87
gig121212 <gitpractice016@gmail.com>
2023-05-09 00:00:30 +0900

    leetcode minimum sum not finished

--- minimun_sum.py
+++ minimun_sum.py
@@ -13,3 +13,14 @@ Return the minimum possible sum of new1 and new2.
 class Solution:
     def minimumSum(self, num: int) -> int:
         pass
+
+'''
+class Solution{
+public:
+    int minimumSum(int num){
+        string s = to_string(num);
+        sort(s.begin(), s.end());
+        int res = (s[0] - '0' + s[1] - '0') * 10 + s[2] - '0' + s[3] - '0';
+        return res;
+    }
+};'''
