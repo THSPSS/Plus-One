10408a6dc6d2db59f1b1bec0b3e09343e75d73f6
gig121212 <sunyeongbang98@gmail.com>
2023-08-15 21:03:47 +0900

    update longest common prefix

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -17,17 +17,25 @@ Output: ""
 Explanation: There is no common prefix among the input strings.
 
 
+check min string from each word
+and loop through word from start
+if first word of each word is not same
+then return empty string
+otherwise compare all word from start until there is different
+
 '''
 from typing import List
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        string = ""
         list_min = min([len(i) for i in strs])
         print(f"list_min {list_min}")
+        #loop through each vocabs with for loop (with min string from words)
         for i in range(list_min-1):
             if strs[0][i] == strs[1][i] == strs[2][i]:
                 return strs[0][i]
-        return "None"
+        return string
 
 
 
