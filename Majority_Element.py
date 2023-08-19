7d5c8515a3bff054975d0a7641e59e47934af118
gig121212 <sunyeongbang98@gmail.com>
2023-08-19 14:48:12 +0900

    update majority element

--- Majority_Element.py
+++ Majority_Element.py
@@ -19,4 +19,11 @@ from typing import List
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         limit = len(nums) / 2
-        return limit
+        unique_values = []
+        for x in nums:
+            if x not in unique_values:
+                unique_values.append(x)
+
+        for j in unique_values:
+            if nums.count(j) > limit:
+                return j
