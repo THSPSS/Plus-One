b531e2bf5fb7e4526ffad90caa01f9305e363400
gig121212 <sunyeongbang98@gmail.com>
2023-08-20 22:55:29 +0900

    update majority element

--- Majority_Element.py
+++ Majority_Element.py
@@ -13,6 +13,18 @@ Example 2:
 Input: nums = [2,2,1,1,1,2,2]
 Output: 2
 
+
+other solution
+
+Intuition:
+The intuition behind this approach is that if an element occurs more than n/2 times in the array (where n is the size of the array), it will always occupy the middle position when the array is sorted. Therefore, we can sort the array and return the element at index n/2.
+
+Explanation:
+The code begins by sorting the array nums in non-decreasing order using the sort function from the C++ Standard Library. This rearranges the elements such that identical elements are grouped together.
+Once the array is sorted, the majority element will always be present at index n/2, where n is the size of the array.
+This is because the majority element occurs more than n/2 times, and when the array is sorted, it will occupy the middle position.
+The code returns the element at index n/2 as the majority element.
+
 '''
 from typing import List
 
@@ -27,3 +39,8 @@ class Solution:
         for j in unique_values:
             if nums.count(j) > limit:
                 return j
+
+    def ohterSolutionMajorityElement(self, nums: List[int]) -> int:
+        nums.sort()
+        n = len(nums)
+        return nums[n // 2]
