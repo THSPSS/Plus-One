8068f54ed2804a3e2a2aa6ad37fcb86ee22ce4ec
gig121212 <sunyeongbang98@gmail.com>
2023-11-28 22:38:58 +0900

    update A+B

---
+++ A+B.py
@@ -0,0 +1,9 @@
+
+
+#frist split A and B by " "
+a,b = input().split()
+print(f"numbers {a} {b}")
+print(int(a) + int(b))
+#so the two number added on list as String
+
+#add two String using int conversion
