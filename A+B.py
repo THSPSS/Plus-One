7c413cf2f9d7b6ad5f44d9a0fdd2a24c5b0c62dc
gig121212 <sunyeongbang98@gmail.com>
2023-11-29 07:40:17 +0900

    update A+B

--- A+B.py
+++ A+B.py
@@ -2,8 +2,10 @@
 
 #frist split A and B by " "
 a,b = input().split()
-print(f"numbers {a} {b}")
 print(int(a) + int(b))
 #so the two number added on list as String
-
 #add two String using int conversion
+
+#using map to convert input split string to int
+a,b = map(input().split())
+print(a+b)
