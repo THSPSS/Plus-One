0625f104d48fce3fa354018d76d324c2fed0e463
gig121212 <sunyeongbang98@gmail.com>
2023-12-26 07:32:33 +0900

    add integer to roman

--- Integer_to_Roman.py
+++ Integer_to_Roman.py
@@ -4,6 +4,22 @@ For example, 2 is written as II in Roman numeral, just two one's added together.
 Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
 
 
+Example 1:
+
+Input: num = 3
+Output: "III"
+Explanation: 3 is represented as 3 ones.
+Example 2:
+
+Input: num = 58
+Output: "LVIII"
+Explanation: L = 50, V = 5, III = 3.
+Example 3:
+
+Input: num = 1994
+Output: "MCMXCIV"
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
 '''
 
 
@@ -11,4 +27,5 @@ class Solution:
     def intToRoman(self, num: int) -> str:
         #check number
         #check if number is smaller than other
+        #each number divide with each section
         return
