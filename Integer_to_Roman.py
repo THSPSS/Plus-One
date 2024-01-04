28e0b4f522e561eeb5bcbb3f8512195d2f2b2a22
gig121212 <sunyeongbang98@gmail.com>
2024-01-05 07:43:49 +0900

    add integer to roman

--- Integer_to_Roman.py
+++ Integer_to_Roman.py
@@ -45,7 +45,9 @@ class Solution:
         for i in range(len(num)):
             count += 1
             print(num / 1000)
+            result = num / 1
             #multiply "M" three times
+
 
 
         return
